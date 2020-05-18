ce25eda3dd8578aa11be629f6323f37c3d71e9f1
Hugo Felipe Torrico Marquez <htorrico@truenorthcorporation.com>
2020-05-18 12:00:47 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio09/Ejercicio09/CustomEntry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Ejercicio09
+{
+    public class CustomEntry : Entry
+    {
+        CustomEntry()
+        {
+            this.Text = "Texto de prueba";
+            
+        }
+    }
+}
+
+
+
+namespace ControlesPersonalizados
+{
+    public class CustomEntry : Entry
+    {
+    }
+}
+
---
+++ Ejercicio09/Ejercicio09/MainPage.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio09
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Item1.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ExplicitDemo());
+
+            };
+            Item2.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ExplicitCode());
+
+            };
+            Item3.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ExplicitControlDemo());
+
+            };
+            Item4.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ExplicitControlCode());
+
+            };
+            Item5.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ImplicitDemo());
+
+            };
+            Item6.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ImplicitCode());
+
+            };
+            Item7.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ImplicitControlDemo());
+
+            };
+        
+            Item9.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new GlobalDemo());
+
+            };
+            Item10.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new GlobalCode());
+
+            };
+            Item11.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new InheritanceDemo());
+
+            };
+            Item12.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new InheritanceCode());
+
+            };
+            Item13.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new DynamicDemo());
+
+            };
+            Item14.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new DynamicCode());
+
+            };
+            Item15.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ClassDemo());
+
+            };
+        }
+    }
+}
